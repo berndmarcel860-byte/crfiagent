71e4ffe31097383b860c73d0f0a6461d5bed3247
berndmarcel860-byte <berndmarcel860@gmail.com>
2026-03-02 06:23:34 +0100

    Delete add_onboarding_email_template.sql

--- add_onboarding_email_template.sql
+++
@@ -1,3 +0,0 @@
--- Add onboarding completion email template
-INSERT INTO email_templates (name, subject, body, variables, created_at, updated_at) 
-VALUES ('onboarding_completed', 'Willkommen - Verifizierung erforderlich', '<!DOCTYPE html><html><body>Email content here</body></html>', 'user_name,company_name', NOW(), NOW());
